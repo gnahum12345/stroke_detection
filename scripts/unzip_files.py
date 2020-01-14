6099ee50ab9ae879b4c49da556220ef9e85ce9c5
Gabriel Nahum <gnahum12@login1.(none)>
2020-01-14 13:22:45 -0800

    Initial Commit

---
+++ scripts/unzip_files.py
@@ -0,0 +1,26 @@
+import sys
+sys.path.insert(1, '../')
+import os
+import gzip
+import shutil
+
+from global_utils import *
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--home_directory', '-hd', '-dir', type=str, default=None)
+    parser.add_argument('--remove_file', '-rm', action='store_true')
+    args = parser.parse_args()
+    params = vars(args)
+    if params['home_directory'] == None:
+        home_directory = input('which folder would you like to unzip all files? \n')
+    else:
+        home_directory = params['home_directory']
+    remove_file = params['remove_file']
+    unzip_data(home_directory,remove_file)
+
+if __name__ == '__main__':
+    main()
